99fe739746e75c61b745c92ba6480c087e9da68e
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-02-22 14:39:20 +0500

    Update Bubble_sort.py

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -12,10 +12,15 @@ Bubble_sort(lst)
 print(lst)
 
 def inefficient_bubble_sort(lst):
-    for i in range(len(lst) - 1):
-        for j in range(len(lst) - 1):
-            if lst[j] > lst[j + 1]:
-                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    for i in range(len(lst) - 1): # n passes
+        for j in range(len(lst) - 1): # n passes
+            if lst[j] > lst[j + 1]: # n comparisons
+                lst[j], lst[j + 1] = lst[j + 1], lst[j] 
+
+'''
+O(n^2) time complexity worst case
+O(n) time complexity best case
+'''
 
 lst = [6, 4, 9, 2, 3, 1, 5, 8, 7]
 inefficient_bubble_sort(lst)
