739b93ada7a77a7a62fc5c51cfd67b7e741b0b19
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-02-22 14:39:43 +0500

    Update Insert_sort.py

--- Insert_sort.py
+++ Insert_sort.py
@@ -1,12 +1,16 @@
 def Insert_sort(lst):
-    for cn in range(1, len(lst)):
-        temp = lst[cn]
-        pointer = cn - 1
-        while pointer > -1 and lst[pointer] > temp:
-            lst[pointer + 1] = lst[pointer]
-            pointer -= 1
-        lst[pointer + 1] = temp
-
+    for cn in range(1, len(lst)): # n + 1 passes
+        temp = lst[cn] # n assignments
+        pointer = cn - 1 # n assignments
+        while pointer > -1 and lst[pointer] > temp: # n + n comparisons
+            lst[pointer + 1] = lst[pointer] # n assignments
+            pointer -= 1 # n assignments
+        lst[pointer + 1] = temp # n assignments
+    
+'''
+O(n^2) time complexity 
+'''
+        
 lst = [6, 4, 9, 2, 3, 1, 5, 8, 7]
 Insert_sort(lst)
 print(lst)
