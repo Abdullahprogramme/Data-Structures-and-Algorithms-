92cc849bcaa2cceacbbc2a5064ae28337537ecc8
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-02-22 14:39:58 +0500

    Update Selection_sort.py

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,10 +1,15 @@
 def Selection_sort(lst):
-    for i in range(len(lst) - 1):
-        min = i
-        for j in range(i+1, len(lst)):
+    for i in range(len(lst) - 1): # n passes
+        min = i # n assignments
+        for j in range(i+1, len(lst)): # n - i comparisons
             if lst[j] < lst[min]:
                 min = j
         lst[i], lst[min] = lst[min], lst[i]
+        
+'''
+O(n^2) time complexity worst case
+O(n) time complexity best case
+'''
 
 lst = [6, 4, 9, 2, 3, 1, 5, 8, 7]
 Selection_sort(lst)
